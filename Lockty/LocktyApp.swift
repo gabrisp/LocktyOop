9e578768ded98c7a6a153106036a496a4ff699bb
Gabriel <gabrielsanpal@gmail.com>
2026-08-25 17:15:09 +0200

    Initial Commit

---
+++ Lockty/LocktyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocktyApp.swift
+//  Lockty
+//
+//  Created by Gabrisp on 25/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct LocktyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
